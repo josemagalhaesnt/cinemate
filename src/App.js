fe585542ed2e77825087c22a2b3dd06e0a0d32a1
José Magalhães <zneto@live.com>
2020-11-23 21:09:16 -0300

    Migrated to MovieDB API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import moviesAPI from './api/moviesAPI';
 import MovieList from './components/MovieList';
 import { createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core';
 import Logo from './components/Logo/index';
+import { GlobalProvider } from 'context/GlobalState';
 
 
 export default function App() {
@@ -22,23 +23,31 @@ export default function App() {
   const [movies, setMovies] = useState([]);
 
   const onSearchSubmit = async (term) => {
-    const response = await moviesAPI.get('', {
-      params: { s: term, type: 'movie' || 'series' },
+    const response = await moviesAPI.get('/multi', {
+      params: { 
+        query: term, 
+        api_key: '4ac686527ef22a8789e8c299a8d5f44a',
+        page: 1,
+        include_adult: false,
+        language: 'pt-BR', 
+      },
     });
 
     console.log(response.data);
 
-    setMovies(response.data.Search);
+    setMovies(response.data.results.filter(m => m.media_type === 'movie' || 'tv'));
   };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalProvider>
       <div className="App">
         <Logo />
         <SearchBar onSubmit={onSearchSubmit} />
         <MovieList movies={movies} />
       </div>
+      </GlobalProvider>
     </ThemeProvider>
   );
 }
