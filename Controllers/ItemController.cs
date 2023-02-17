51e3b03077aa10c31f5d73e6a19426c32e462b41
José Magalhães <jose_magalhaes@atlantico.com.br>
2023-02-17 17:32:50 -0300

    WIP: Item model and controller implementation

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using Cinemate.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinemate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = new List<Item>
+            {
+                new Item { Id = 1, Title = "White Chicks", ReleaseDate = new DateTimeOffset(new DateTime(2004, 08, 27)), OriginalTitle = "White Chicks", PosterUrl = "" }
+            };
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace Cinemate.Models
+{
+    public class Item
+    {
+        public Item(int id, string title, DateTimeOffset releaseDate, string? originalTitle, string? posterUrl)
+        {
+            Id = id;
+            Title = title;
+            ReleaseDate = releaseDate;
+            OriginalTitle = originalTitle;
+            PosterUrl = posterUrl;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset ReleaseDate { get; set; }
+        public string? OriginalTitle { get; set; }
+        public string? PosterUrl { get; set; }
+
+    }
+}
